897bcb2b7e5bae05f621b49fc8d5fe22171a27e8
Srikanth Karanam <srikanth.karanam336@gmail.com>
2024-09-06 09:11:24 +0530

    Adding files 06th spe

---
+++ src/sept/ex_06092024/Lab006.java
@@ -0,0 +1,27 @@
+package sept.ex_06092024;
+
+public class Lab006 {
+
+    public static void main(String[] args) {
+        // Single line comment example
+        System.out.println("Srikanth");
+      //  System.out.println("Srikanth");
+
+    /* multi line comments
+    * Author: Srikanth
+    * Course : Java
+    * */
+
+/*        System.out.println("Srikanth");
+        System.out.println("Srikanth");
+        System.out.println("Srikanth");*/
+
+        System.out.println(2*2);
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2/2);
+
+    }
+
+}
+
