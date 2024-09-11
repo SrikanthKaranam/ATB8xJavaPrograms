e48e6da30c867c314acb0dbec85f0332b3ff565c
Srikanth Karanam <srikanth.karanam336@gmail.com>
2024-09-11 08:12:04 +0530

    adding the data types

---
+++ src/sept/ex_11092024/Lab010_DataTypes.java
@@ -0,0 +1,18 @@
+package sept.ex_11092024;
+
+public class Lab010_DataTypes {
+
+    boolean is_married = true;
+    byte as = -128;
+
+    // 1 Byte = 8 bits
+
+    byte b = 127; // 8 bits, 1 Byte
+    short s = 32767 ; // 16 bits , 2 Byte, (2^15-1)
+    int i = 3222; // 32 bits,  4 Byte, (2^31-1)
+    long l = 64111111L ; // 64 bits, 8 Byte (2^63-1)
+    char c = 1; // 16 bits, 2 Byte, (2^15-1)
+    float f = 32; // 32 bits, 4 Byte
+    double d = 64; // 64 bits, 8 byte
+
+}
